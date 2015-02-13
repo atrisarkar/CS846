588eabe2abfa460bdf284c30853f2c1a14f51bba
Atri Sarkar <atrisarkar@gmail.com>
2015-02-13 03:22:45 -0500

    Removing OAuth token

--- src/main/java/GitHubConstants.java
+++ src/main/java/GitHubConstants.java
@@ -7,7 +7,7 @@ public class GitHubConstants {
 
 	public static final String USER = "atrisarkar";
 	public static final String COMMIT_URL = "https://api.github.com/repos/eatnumber1/google-gson/commit";
-	public static String OAUTH = "0f0e56a561d65b8423307ae848184610f3e474ad";
+	public static String OAUTH = "secret :)";
 	public static final String AUTH_URL = "https://api.github.com/?access_token="+OAUTH;
 	public static final String REPOSITORY_PATH = "elasticsearch/elasticsearch/";
 	
