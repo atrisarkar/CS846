8c1cb203c8d661525fc4c4e5738236c665c316ff
Atri Sarkar <atrisarkar@gmail.com>
2015-02-13 01:56:47 -0500

    User script for Github tab

---
+++ src/main/resources/CS846.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name        CS846
+// @namespace   http://github.com/elasticsearch/elasticsearch/pull
+// @include     https://github.com/elasticsearch/elasticsearch/pull/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @version     1
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+$(document).ready(showAlert);
+
+
+
+function showAlert() {
+  var t =  (document.getElementsByClassName("tabnav-tabs")[0]).children[2].cloneNode(true);
+  console.log (t);
+ 
+  var related_bucket_content = document.getElementById("toc").cloneNode(true);
+  related_bucket_content.setAttribute("id","toc_related_files");
+  related_bucket_content.removeChild(related_bucket_content.children[0]);
+  related_bucket_content.children[0].setAttribute("id","warning_label");
+  related_bucket_content.children[1].setAttribute("id","file_list");
+  
+  var related_bucket = document.getElementById("files_bucket").cloneNode(false);
+  related_bucket.setAttribute("id","related_bucket");
+  related_bucket.appendChild(related_bucket_content);
+  var parent_of_bucket = document.getElementsByClassName("view-pull-request clearfix js-issues-results")[0].appendChild(related_bucket);
+  
+  
+  
+  t.children[0].setAttribute("data-container-id","related_bucket");
+  var ih = t.children[0].innerHTML.replace("Files changed","Related Files");
+  t.children[0].innerHTML = ih;
+  document.getElementsByClassName("tabnav-tabs")[0].appendChild(t);
+  
+  
+  GM_xmlhttpRequest({
+  method: "GET",
+  url: "http://httpbin.org/get",
+  onload: function(response) {
+    updateData(response);
+  }
+  });
+  //alert(ih);
+}
+
+function updateData(response) {
+  alert("coming");
+  var warning_label = document.getElementById("warning_label");
+  warning_label.innerHTML = "You might want to review the following files for potential impact";
+  warning_label.style = "background-color:#e8f0f8";
+  document.getElementById("toc_related_files").setAttribute("class","details-collapse table-of-contents js-details-container open");
+}
