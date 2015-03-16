bad8138d8395b26141c3562d46907ea73edeab44
Atri Sarkar <atrisarkar@gmail.com>
2015-03-16 01:09:33 -0400

    New file

---
+++ src/main/java/com/cs846/code/json/FileEntry.java
@@ -0,0 +1,49 @@
+package com.cs846.code.json;
+
+public class FileEntry {
+	
+	
+	
+	String file;
+	
+	String message;
+	
+	String support;
+	
+	String confidence;
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getSupport() {
+		return support;
+	}
+
+	public void setSupport(String support) {
+		this.support = support;
+	}
+
+	public String getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(String confidence) {
+		this.confidence = confidence;
+	}
+
+	
+
+}
